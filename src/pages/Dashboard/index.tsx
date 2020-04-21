be90c2c5a29663d56e6175524b6aae8c9178ace1
elvinciqueira <elvinciqueira@gmail.com>
2020-04-20 22:44:50 -0300

    feat: estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
-};
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Logo" />
+    <Title>Explore repositórios no GitHub</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="#teste">
+        <img
+          src="https://avatars0.githubusercontent.com/u/42623554?s=460&u=a3a49601be691e3addf7295621fbbf6acf2ed5da&v=4"
+          alt="Elvin Ciqueira"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
